ab7924807cc95ade6bf0c93932790b4cbc3e24ba
sahil407x29 <sahil407x29@users.noreply.github.com>
2026-06-22 23:19:41 +0800

    learnt about types of declaration in go and types of placeholders used in go also about the implicit and explicit declarations and the walrus operator

---
+++ 02variables/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+const LoginToken = "sahil" //public - accessible from anywhere
+func main() {
+	// fmt.Println("hello")
+	// var Username string = "Sahil";
+	// fmt.Println(Username)
+	// fmt.Printf("Variable is a type of %T" ,Username)
+	// var anotherVar int ;
+	// fmt.Printf("variable type and values is %T and %v" ,anotherVar,anotherVar)
+
+	// implicit type
+	// var website = "www.fast.com"
+	// fmt.Println(website)
+
+    //:= (walrus operator ) = "Create a new variable and assign a value to it
+	// myName := "Sahil";
+	// fmt.Printf((myName))
+
+	fmt.Printf(LoginToken)
+}
+
